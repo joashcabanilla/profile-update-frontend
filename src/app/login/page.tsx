b2d1d36d302ec606ec3dae1865cf34737d690e57
joashcabanilla <joash0199@gmail.com>
2025-06-03 22:33:50 +0800

    revert authentication

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,10 @@
 //metadata
 import type { Metadata } from "next";
-import { signIn } from "@/auth";
 
 export const metadata: Metadata = {
     title: "Login"
 };
 
 export default function Login() {
-    return (
-        <form
-            action={async (formData) => {
-                "use server";
-                await signIn("credentials", formData);
-            }}
-        >
-            <label>
-                Email
-                <input name="email" type="email" />
-            </label>
-            <label>
-                Password
-                <input name="password" type="password" />
-            </label>
-            <button>Sign In</button>
-        </form>
-    );
+    return (<h1>login</h1>);
 }
