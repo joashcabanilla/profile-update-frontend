fed935adeb7887ddafeefc85cb5d182b936b4692
joashcabanilla <joash0199@gmail.com>
2025-10-29 13:58:48 +0800

    add birthdate

--- src/components/home/Step3.tsx
+++ src/components/home/Step3.tsx
@@ -3,6 +3,7 @@
 //hooks
 import React, { useRef, useState, useTransition } from "react";
 import { toast } from "sonner";
+import { format, isValid, parse } from "date-fns";
 
 //style utils and variants
 import { cn } from "@/lib/utils";
@@ -17,6 +18,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import ProfileUpdated from "@/components/home/ProfileUpdated";
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 
 //context global state
 import { useMemberContext } from "@/context/member-context";
@@ -27,15 +30,21 @@ import { Member, updateProfileInput } from "@/types/type";
 export default function Step3() {
   const { memberId, searchedMember, setStep, stepCompleted, setStepCompleted } = useMemberContext();
   const profileFormRef = useRef<HTMLFormElement>(null);
-  const [cpnumberRef, emailRef, tinRef] = [
+  const [cpnumberRef, emailRef, tinRef, birthdateRef] = [
+    useRef<HTMLInputElement>(null),
     useRef<HTMLInputElement>(null),
     useRef<HTMLInputElement>(null),
     useRef<HTMLInputElement>(null)
   ];
+  const birthdatePrevRef = useRef("");
 
   const [cpnumberState, setCpnumberState] = useState<string>("");
   const [emailState, setEmailState] = useState<string>("");
   const [tinState, setTinState] = useState<string>("");
+  const [calendarOpen, setCalendarOpen] = useState<boolean>(false);
+  const [birthdateInput, setBirthdateInput] = useState<string>("");
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>();
+  const [month, setMonth] = useState<Date>(new Date());
 
   const memberData: Member[] = searchedMember.filter((data) => data.id == memberId && data);
 
@@ -124,9 +133,10 @@ export default function Step3() {
       id: id,
       cpNumber: cpnumberState,
       email: emailState,
-      tinNumber: tinState === "" ? undefined : tinState
+      tinNumber: tinState === "" ? undefined : tinState,
+      birthdate: birthdateInput === "" ? undefined : new Date(birthdateInput)
     };
-
+    
     startTransition(async () => {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL!}/member/updateMember`, {
         method: "POST",
@@ -145,6 +155,60 @@ export default function Step3() {
         toast.error(errorMessage, { duration: 3000 });
       }
     });
+  };
+
+  const handleBirhtdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCalendarOpen(true);
+    const input = e.target.value;
+    const inputPrev = birthdatePrevRef.current;
+
+    let dateInput = input.replace(/\D/g, "");
+
+    if (input.length < inputPrev.length) {
+      setBirthdateInput(input);
+      birthdatePrevRef.current = input;
+      setSelectedDate(undefined);
+      return;
+    }
+
+    if (dateInput.length > 1) {
+      dateInput = dateInput.slice(0, 2) + "/" + dateInput.slice(2);
+    }
+    if (dateInput.length > 4) {
+      dateInput = dateInput.slice(0, 5) + "/" + dateInput.slice(5);
+    }
+    const formatted = dateInput.slice(0, 10);
+    setBirthdateInput(formatted);
+    birthdatePrevRef.current = formatted;
+
+    const parsedDate = parse(formatted, "MM/dd/yyyy", new Date());
+    if (isValid(parsedDate)) {
+      setMonth(parsedDate);
+      setSelectedDate(parsedDate);
+    } else {
+      setMonth(new Date());
+      setSelectedDate(undefined);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      birthdateRef.current?.blur();
+      setCalendarOpen(false);
+    }
+  };
+
+  const handleDayPickerSelect = (date: Date | undefined) => {
+    if (!date) {
+      setBirthdateInput("");
+      setMonth(new Date());
+      setSelectedDate(undefined);
+    } else {
+      setSelectedDate(date);
+      setMonth(date);
+      setBirthdateInput(format(date, "MM/dd/yyyy"));
+    }
   };
 
   return stepCompleted ? (
@@ -243,6 +307,73 @@ export default function Step3() {
               </div>
             )
           )}
+          <div className="*:not-first:mt-2">
+            <Label htmlFor="birthdate">Birthdate</Label>
+            <div className="relative">
+              <Popover open={calendarOpen} onOpenChange={setCalendarOpen}>
+                <PopoverTrigger asChild>
+                  <Input
+                    ref={birthdateRef}
+                    id="birthdate"
+                    placeholder="mm/dd/yyyy"
+                    type="text"
+                    maxLength={10}
+                    onInput={handleBirhtdate}
+                    onKeyDown={handleKeyDown}
+                    value={birthdateInput}
+                    className="peer pe-9"
+                  />
+                </PopoverTrigger>
+                <PopoverContent
+                  avoidCollisions={false}
+                  side="top"
+                  onOpenAutoFocus={(event: Event) => {
+                    event.preventDefault();
+                    if (birthdateInput.length == 10) {
+                      const parsedDate = parse(birthdateInput, "MM/dd/yyyy", new Date());
+                      if (isValid(parsedDate)) {
+                        setMonth(parsedDate);
+                        setSelectedDate(parsedDate);
+                      } else {
+                        setMonth(new Date());
+                        setSelectedDate(undefined);
+                      }
+                    } else {
+                      setMonth(new Date());
+                      setSelectedDate(undefined);
+                    }
+                  }}
+                  className="w-fit p-1 sm:w-auto sm:p-2"
+                  align="start"
+                >
+                  <Calendar
+                    mode="single"
+                    selected={selectedDate}
+                    month={month}
+                    onMonthChange={setMonth}
+                    onSelect={handleDayPickerSelect}
+                  />
+                </PopoverContent>
+              </Popover>
+              <button
+                className={cn(
+                  button({
+                    variant: "closeIcon"
+                  }),
+                  birthdateInput ? "cursor-pointer opacity-100" : "pointer-events-none opacity-0"
+                )}
+                aria-label="Submit search"
+                type="submit"
+                onClick={() => {
+                  setBirthdateInput("");
+                  setMonth(new Date());
+                  setSelectedDate(undefined);
+                }}
+              >
+                <CircleX size={20} aria-hidden="true" />
+              </button>
+            </div>
+          </div>
         </form>
 
         <div className="mt-2 flex flex-col gap-2 sm:flex-row-reverse sm:justify-between">
